238709c3a676adb89a407aa914b042de79cee1af
Jan Hesters <jan.hesters@rwth-aachen.de>
2018-09-17 16:05:01 +0300

    Add localization.

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import LocalizedStrings from 'react-native-localization';
+
+let strings = new LocalizedStrings({
+  en: {
+    welcome: "Welcome to React Native!",
+    instructions: "To get started, edit App.tsx",
+    instructionsIOS: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+    instructionsAndroid: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
+  },
+  de: {
+    welcome: "Willkommen zu React Native!",
+    instructions: "Um anzufangen, bearbeite App.tsx",
+    instructionsIOS: "Drücke Cmd+R zum Neuladen,\n" + "Cmd+D oder schüttle für das Entwickler Menü",
+    instructionsAndroid: "Tippe doppelt auf R auf deiner Tastatur um neu zu laden,\n" + "Schüttle oder drücke den Menü Knopf für das Entwickler Menü"
+  }
+})
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
+  ios: strings.instructionsIOS,
+  android: strings.instructionsAndroid
 });
 
 interface Props {}
@@ -11,8 +27,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+        <Text style={styles.welcome}>{strings.welcome}</Text>
+        <Text style={styles.instructions}>{strings.instructions}</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
